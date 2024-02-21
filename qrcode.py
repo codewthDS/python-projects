73e472dcb01510a1d0e320fabe8bf5c9df6f30f0
Rahul <155246973+codewthDS@users.noreply.github.com>
2024-02-21 20:00:27 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -1,3 +1,4 @@
+'''First you need to install qr code library on your computer ...then  follow the steps below:'''
 import qrcode as qr
 img=qr.make("https://github.com/codewthDS")
 img.save("Gitqr.jpg")
